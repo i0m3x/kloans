2bab425bc97c0ca47e0127cd178beb4180ea156a
lexiconPDX <chaungemail@gmail.com>
2020-08-21 11:36:53 -0700

    various higher order methods

--- koans/AboutHigherOrderFunctions.js
+++ koans/AboutHigherOrderFunctions.js
@@ -9,6 +9,7 @@ describe("About Higher Order Functions", function () {
 
   it("should use filter to return array items that meet a criteria", function () {
     var numbers = [1,2,3];
+    //filter() calls a provided callback function once for each element in an array, and constructs a new array of all the values for which callback returns a value that coerces to true. callback is invoked only for indexes of the array which have assigned values; it is not invoked for indexes which have been deleted or which have never been assigned values. Array elements which do not pass the callback test are simply skipped, and are not included in the new array.
     var odd = _(numbers).filter(function (x) { return x % 2 !== 0 });
 
     expect(odd).toEqual([1,3]);
@@ -18,19 +19,24 @@ describe("About Higher Order Functions", function () {
 
   it("should use 'map' to transform each element", function () {
     var numbers = [1, 2, 3];
+    //map calls a provided callback function once for each element in an array, in order, and constructs a new array from the results. callback is invoked only for indexes of the array which have assigned values (including undefined).
     var numbersPlus1 = _(numbers).map(function(x) { return x + 1 });
 
-    expect(numbersPlus1).toEqual(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
+    expect(numbersPlus1).toEqual([2,3,4]);
+    expect(numbers).toEqual([1, 2, 3]);
   });
 
   it("should use 'reduce' to update the same result on each iteration", function () {
     var numbers = [1, 2, 3];
+
+    //Your reducer function's returned value is assigned to the accumulator, whose value is remembered across each iteration throughout the array, and ultimately becomes the final, single resulting value.
+
+
     var reduction = _(numbers).reduce(
             function(/* result from last call */ memo, /* current */ x) { return memo + x }, /* initial */ 0);
 
-    expect(reduction).toBe(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
+    expect(reduction).toBe(6);
+    expect(numbers).toEqual([1,2,3]);
   });
 
   it("should use 'forEach' for simple iteration", function () {
@@ -39,11 +45,11 @@ describe("About Higher Order Functions", function () {
     var isEven = function (item) {
       msg += (item % 2) === 0;
     };
-
+//forEach() calls a provided callback function once for each element in an array in ascending order. It is not invoked for index properties that have been deleted or are uninitialized. 
     _(numbers).forEach(isEven);
 
-    expect(msg).toEqual(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
+    expect(msg).toEqual('falsetruefalse');
+    expect(numbers).toEqual([1,2,3]);
   });
 
   it("should use 'all' to test whether all items pass condition", function () {
@@ -52,8 +58,8 @@ describe("About Higher Order Functions", function () {
 
     var isEven = function(x) { return x % 2 === 0 };
 
-    expect(_(onlyEven).all(isEven)).toBe(FILL_ME_IN);
-    expect(_(mixedBag).all(isEven)).toBe(FILL_ME_IN);
+    expect(_(onlyEven).all(isEven)).toBe(true);
+    expect(_(mixedBag).all(isEven)).toBe(false);
   });
 
   it("should use 'any' to test if any items passes condition" , function () {
@@ -62,18 +68,21 @@ describe("About Higher Order Functions", function () {
 
     var isEven = function(x) { return x % 2 === 0 }; //x is arg, returns true if even num
 
-    expect(_(onlyEven).any(isEven)).toBe(FILL_ME_IN);
-    expect(_(mixedBag).any(isEven)).toBe(FILL_ME_IN);
+    expect(_(onlyEven).any(isEven)).toBe(true);
+    expect(_(mixedBag).any(isEven)).toBe(true);
   });
 
   it("should use range to generate an array", function() {
-      expect(_.range(3)).toEqual(FILL_ME_IN); //let's js do range - will create array
-      expect(_.range(1, 4)).toEqual(FILL_ME_IN);
-      expect(_.range(0, -4, -1)).toEqual(FILL_ME_IN);
+    //https://github.com/dhtlee/mrdavidlaing-javascript-koans-solutions/blob/master/AboutHigherOrderFunctions.js could not find first test
+      expect(_.range(3)).toEqual([0,1,2]); //let's js do range - will create array
+      expect(_.range(1, 4)).toEqual([1,2,3]);
+      expect(_.range(0, -4, -1)).toEqual([0, -1, -2, -3]);
   });
 
   it("should use flatten to make nested arrays easy to work with", function() {
-      expect(_([ [1, 2], [3, 4] ]).flatten()).toEqual(FILL_ME_IN);
+
+    //The flat() method creates a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+      expect(_([ [1, 2], [3, 4] ]).flatten()).toEqual([1, 2, 3, 4]);
   });
 
   it("should use chain() ... .value() to use multiple higher order functions", function() {
@@ -83,7 +92,8 @@ describe("About Higher Order Functions", function () {
                        .reduce(function (sum, x) { return sum + x })//starts at 0  single value will be o/p = 6
                        .value(); //Extracts the value of a wrapped object.
 
-      expect(result).toEqual(FILL_ME_IN);
+      expect(result).toEqual(6); //reduce gives single vale
+      // yay, finally completed this on 21 Aug 2020
   });
 
 });
